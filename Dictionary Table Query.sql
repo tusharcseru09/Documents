fe82eb687cd7ada3188658a2d36022228991b470
tusharcseru09 <tushar.cseru09@gmail.com>
2017-10-30 00:08:42 +0600

    Update Dictionary Table Query.sql

--- Dictionary Table Query.sql
+++ Dictionary Table Query.sql
@@ -11,4 +11,15 @@ add column synonyms varchar(500);
 
 alter table mydictionary
 add column antonyms varchar(500);
+
+
+insert into mydictionary(word,meaning,description,synonyms,antonyms,tag,urlLink)
+values ('Realm',
+        'A kingdom',
+        'A field or domain of activity or interest',
+        null,
+        null,
+        null,
+        null
+       );
  
